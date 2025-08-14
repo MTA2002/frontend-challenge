17e265fd49d891db081ea602efe1f4571c4a14c8
MTA2002 <mahfouz.teyib@aastustudent.edu.et>
2025-08-14 21:56:55 +0300

    modified the border radius in profile-card and security card

--- components/profile-card.tsx
+++ components/profile-card.tsx
@@ -8,13 +8,13 @@ import EditProfileDialog from "./edit-profile-dialog";
 const ProfileCard = () => {
   return (
     <div className="rounded bg-[#FFFFFF]">
-      <div className="flex items-center justify-between py-4  px-6 border border-[#F2F2F2]">
+      <div className="flex items-center justify-between py-4  px-6 border border-[#F2F2F2] rounded-t">
         <p className="font-medium text-lg text-[#222222]">Profile Details</p>
 
         <EditProfileDialog />
       </div>
 
-      <div className="p-6 border flex border-[#F2F2F2] flex-col  gap-4">
+      <div className="p-6 border flex border-[#F2F2F2] flex-col  gap-4 rounded-b">
         <div className="flex flex-col md:flex-row gap-4">
           <ProfileField label="First Name" value="John" />
           <ProfileField label="Last Name" value="Doe" />
--- components/security-card.tsx
+++ components/security-card.tsx
@@ -6,12 +6,12 @@ import EditProfileDialog from "./edit-profile-dialog";
 const SecurityCard = () => {
   return (
     <div className="rounded bg-[#FFFFFF]">
-      <div className="flex items-center justify-between py-4  px-6 border border-[#F2F2F2]">
+      <div className="flex items-center justify-between py-4  px-6 border border-[#F2F2F2] rounded-t">
         <p className="font-medium text-lg text-[#222222]">Security</p>
         <EditProfileDialog />
       </div>
 
-      <div className="p-6 border border-[#F2F2F2] flex justify-between">
+      <div className="p-6 border border-[#F2F2F2] flex justify-between rounded-b">
         <ProfileField label="Password" value="*********************" />
       </div>
     </div>
